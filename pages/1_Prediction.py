035a7ab21ffe3338e440488ddf2956b7ef891ce8
modiab145 <mt103t1972@gmail.com>
2026-08-01 02:46:40 +0300

    Update 1_Prediction.py

--- pages/1_Prediction.py
+++ pages/1_Prediction.py
@@ -1,5 +1,4 @@
-from Heart_Disease_AI.utils import prediction
-import streamlit as st
+from utils import predictionimport streamlit as st
 from utils.prediction import predict_patient
 from utils.history import save_prediction
 from utils.pdf_report import generate_pdf
