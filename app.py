a4473a263fc2059b259f2421901590063eac8349
modiab145 <mt103t1972@gmail.com>
2026-08-01 13:13:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ st.set_page_config(
 )
 
 import streamlit as st
-col1, col2 = st.columns([1, 4])
 
-with col1:
+with st.sidebar:
+    st.image("Heart_Disease_AI/assets/logo.PNG.jpeg", use_container_width=True)
+    
     st.title("Heart Disease AI")
     
     st.markdown("---")
